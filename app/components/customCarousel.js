14b7d9c857c92edac47f56d7b98c7ec3bd76e65a
Priyosmita <daspriyosmita2003@gmail.com>
2024-09-11 01:43:22 +0530

    carousel changes

--- app/components/customCarousel.js
+++ app/components/customCarousel.js
@@ -1,6 +1,43 @@
 import React from 'react'
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { VscTriangleLeft, VscTriangleRight } from 'react-icons/vsc';
+
+// function for custom slider arrow
+const customArrowStyles = {
+  position: 'absolute',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  zIndex: 2,
+  // backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  color: '#fff',
+  border: 'none',
+  padding: '10px',
+  cursor: 'pointer',
+  fontSize: '24px', // Adjust the size of the icons
+};
+
+const renderArrowPrev = (onClickHandler, hasPrev, label) =>
+  hasPrev && (
+    <button
+      type="button"
+      onClick={onClickHandler}
+      title={label}
+      style={{ ...customArrowStyles, left: 15 }}>
+      <VscTriangleLeft /> 
+    </button>
+  );
+
+const renderArrowNext = (onClickHandler, hasNext, label) =>
+  hasNext && (
+    <button
+      type="button"
+      onClick={onClickHandler}
+      title={label}
+      style={{ ...customArrowStyles, right: 15 }}>
+      <VscTriangleRight /> 
+    </button>
+  );
 
 const customCarousel = () => {
   return (
@@ -9,9 +46,11 @@ const customCarousel = () => {
       autoPlay
       infiniteLoop
       showArrows={true}
+      renderArrowPrev={renderArrowPrev}
+      renderArrowNext={renderArrowNext}
       showIndicators={true}
       interval={3000}>
-      <div>
+      <div className='h-1/3'>
         <video src="\assets\carousel1.mp4" autoPlay loop muted playsInline alt="Slide 1" />
       </div>
       <div>
