45c002e7ae390d7c6ff317442fa5235141403284
Priyosmita <daspriyosmita2003@gmail.com>
2024-09-14 01:47:40 +0530

    error fix

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import React from 'react'
 // components
 import Header from './components/Header';
-import CustomCarousel from './components/customCarousel';
+import customCarousel from './components/customCarousel';
 
 import './globals.css'
 
@@ -11,7 +11,7 @@ const page = () => {
   return (
     <>
     <Header/>
-    <CustomCarousel/>
+    <customCarousel/>
     {/* <Carousel showThumbs={false} autoPlay>
 
     </Carousel> */}
