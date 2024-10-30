959cb4cf37a6dd79300f98a65c706f061c9cb279
Priyosmita <daspriyosmita2003@gmail.com>
2024-10-30 12:27:21 +0530

    comit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 import 'bootstrap/dist/css/bootstrap.min.css'; // for react bootstrap
-import './globals.css'
-import React from 'react'
+import './globals.css';
+import React from 'react';
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,18 +9,22 @@ import LandingPageOptions from './components/LandingPageOptions';
 import MandalaStrip from './components/MandalaStrip';
 import CarouselSlider from './components/CarouselSlider';
 
-
-
 const page = () => {
   return (
-    <div div className='flex flex-col'>
+    <div className='flex flex-col'>
       <Header />
-      <CarouselSlider/>
+
+      {/* Sticky wrapper for Carousel */}
+      <div style={{ position: 'sticky', top: '0', zIndex: '-1' }}>
+        <CarouselSlider />
+      </div>
+
+      {/* Following components */}
       <MandalaStrip />
-      <LandingPageOptions/>
+      <LandingPageOptions />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
