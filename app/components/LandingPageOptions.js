7e5cbad2ffb52adf742b0aebaea056fccae55985
Priyosmita <daspriyosmita2003@gmail.com>
2024-10-30 16:30:33 +0530

    options

--- app/components/LandingPageOptions.js
+++ app/components/LandingPageOptions.js
@@ -7,20 +7,54 @@ const LandingPageOptions = () => {
     return (
         <>
             <MandalaStrip />
-            <div className='min-h-[48vh] bg-cover bg-center'>
+            <div className="relative min-h-[48vh] bg-cover bg-center overflow-hidden">
                 <img
                     src="/assets/bg landing.png"
                     alt="bg img"
+                    className="w-full h-full object-contain"
                 />
-                <div className=''>
-                    <div className='flex flex-row'>
+                
+                <div className="absolute inset-0 flex items-center justify-center space-x-4">
+                    <div className="flex flex-row items-center space-x-2">
                         <div>
                             <img
-                            src="/assets/Mask group 1.png"
-                            alt="first img"
-                        />
+                                src="/assets/Mask group 1.png"
+                                alt="first img"
+                                className="w-[100px] h-[100px] object-cover"
+                            />
                         </div>
                         <div>bhakti</div>
+                    </div>
+                    {/* Add more options here */}
+                    <div className="flex flex-row items-center space-x-2">
+                        <div>
+                            <img
+                                src="/assets/Mask group 2.png"
+                                alt="second img"
+                                className="w-[100px] h-[100px] object-cover"
+                            />
+                        </div>
+                        <div>option 2</div>
+                    </div>
+                    <div className="flex flex-row items-center space-x-2">
+                        <div>
+                            <img
+                                src="/assets/Mask group 3.png"
+                                alt="third img"
+                                className="w-[100px] h-[100px] object-cover"
+                            />
+                        </div>
+                        <div>option 3</div>
+                    </div>
+                    <div className="flex flex-row items-center space-x-2">
+                        <div>
+                            <img
+                                src="/assets/Mask group 4.png"
+                                alt="fourth img"
+                                className="w-[100px] h-[100px] object-cover"
+                            />
+                        </div>
+                        <div>option 4</div>
                     </div>
                 </div>
             </div>
